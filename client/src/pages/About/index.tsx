1ee34a8bb86a77970448937d34e021b51461d3c7
Aleksandr Pankov <nayteruz@gmail.com>
2024-07-27 02:23:17 +0700

    complete pages

--- client/src/pages/About/index.tsx
+++ client/src/pages/About/index.tsx
@@ -1,3 +1,26 @@
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="flex-1 flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center ">
+        <div className="">
+          <h1 className="text-3xl font font-semibold">About</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>Блог о веб-разработке</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo,
+              cupiditate natus? Repudiandae eos laboriosam atque ipsum magnam
+              sapiente unde ducimus adipisci. Nemo sunt impedit officia cumque
+              atque quos id debitis?
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut, fuga
+              nisi adipisci modi sequi eos minus saepe dolor, officia animi
+              voluptas dicta numquam! Excepturi illum aperiam, sunt quos
+              dignissimos minima.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- client/src/pages/Projects/index.tsx
+++ client/src/pages/Projects/index.tsx
@@ -1,3 +1,16 @@
+import { CallToAction } from "../Post/CallToAction";
+
 export const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="flex-1 max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Проекты</h1>
+      <p className="text-md text-gray-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni possimus
+        ipsum voluptas natus quibusdam corporis, officia, temporibus dolorum
+        soluta dicta rem cupiditate voluptates, iure nemo fugiat aut quae?
+        Dolore, dolorem.
+      </p>
+      <CallToAction />
+    </div>
+  );
 };
